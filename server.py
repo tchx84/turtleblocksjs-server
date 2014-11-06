b5fad8be18e5d3be35920b29aa969926399eb3b8
Martin Abente Lahaye <tch@sugarlabs.org>
2014-11-06 11:16:35 -0300

    Specify project id in the URL instead of using headers
    If no project id is specified, the server will return the
    list of all projects in JSON list, ie:
    
    to create or update:
        POST http://turtle.sugarlabs.org/server/tch01.tb
    
    to retrieve:
        GET http://turtle.sugarlabs.org/server/tch01.tb
    
    to list all:
        GET http://turtle.sugarlabs.org/server
    
    Signed-off-by: Martin Abente Lahaye <tch@sugarlabs.org>

--- server.py
+++ server.py
@@ -17,6 +17,7 @@
 # Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301 USA
 
 import os
+import json
 import logging
 import SimpleHTTPServer
 import SocketServer
@@ -39,26 +40,46 @@ def authorize(method):
 def check(method):
     """ put things under control """
     def verify(handler, *args, **kwargs):
-        if not 'x-project-id' in handler.headers or \
-               not isinstance(handler.headers['x-project-id'], str) or \
-               not handler.headers['x-project-id'].find('/') < 0 or \
-               check_if_missing(method, handler):
-            handler.send_response(404, "not found")
+        project_id = get_project_id(handler)
+        if project_id and project_id.find('/') >= 0:
+            handler.send_response(403, 'forbidden')
+            handler.end_headers()
+            return None
+        if project_id and check_if_missing(method, handler):
+            handler.send_response(404, 'not found')
             handler.end_headers()
             return None
         return method(handler, *args, **kwargs)
     return verify
 
 
-def check_if_missing(method, handler):
-    if method.__name__ == 'do_GET' and \
-           not os.path.exists(get_project_path(handler)):
-        return True
-    return False
+def get_project_id(handler):
+    return handler.path.replace('/', '')
 
 
 def get_project_path(handler):
-    return os.path.join(Settings.PROJECTS, handler.headers['x-project-id'])
+    project_id = get_project_id(handler)
+    return os.path.join(Settings.PROJECTS, project_id)
+
+
+def get_all_projects():
+    filenames = []
+    for filename in os.listdir(Settings.PROJECTS):
+        filenames.append(filename)
+    return json.dumps(filenames)
+
+
+def get_one_project(handler):
+    path = get_project_path(handler)
+    with open(path, 'r') as file:
+        return file.read()
+
+
+def check_if_missing(method, handler):
+    if method.__name__ == 'do_GET' and \
+       not os.path.isfile(get_project_path(handler)):
+        return True
+    return False
 
 
 class ServerHandler(SimpleHTTPServer.SimpleHTTPRequestHandler):
@@ -76,33 +97,36 @@ class ServerHandler(SimpleHTTPServer.SimpleHTTPRequestHandler):
     @authorize
     @check
     def do_GET(self):
-        logging.info(self.headers)
-
         self.send_response(200)
         self.send_header("Content-type", "application/json")
         self.end_headers()
 
-        path = get_project_path(self)
-        with open(path, 'r') as file:
-            self.wfile.write(file.read())
+        if get_project_id(self):
+            body = get_one_project(self)
+        else:
+            body = get_all_projects()
+        self.wfile.write(body)
 
     @authorize
     @check
     def do_POST(self):
-        logging.info(self.headers)
-
         self.send_response(200)
         self.end_headers()
 
         content_len = int(self.headers.getheader('content-length', 0))
         content = self.rfile.read(content_len)
-        logging.error(content)
 
         path = get_project_path(self)
         with open(path, 'w') as file:
             file.write(content)
 
-httpd = SocketServer.TCPServer((Settings.ADDRESS,
-                               Settings.PORT),
-                               ServerHandler)
-httpd.serve_forever()
+if __name__ == '__main__':
+    httpd = SocketServer.TCPServer((Settings.ADDRESS,
+                                   Settings.PORT),
+                                   ServerHandler)
+    try:
+        httpd.serve_forever()
+    except KeyboardInterrupt:
+        pass
+
+    httpd.server_close()
